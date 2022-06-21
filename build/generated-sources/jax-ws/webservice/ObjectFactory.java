6a5517197e6c76948ea89636b5f7dc40c7e44095
Sam <rieszein@gmail.com>
2022-06-21 17:42:05 -0500

    DsamiStore CRUD

---
+++ build/generated-sources/jax-ws/webservice/ObjectFactory.java
@@ -0,0 +1,97 @@
+
+package webservice;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the webservice package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Exception_QNAME = new QName("http://WebService/", "Exception");
+    private final static QName _Listar_QNAME = new QName("http://WebService/", "listar");
+    private final static QName _ListarResponse_QNAME = new QName("http://WebService/", "listarResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: webservice
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Exception }
+     * 
+     */
+    public Exception createException() {
+        return new Exception();
+    }
+
+    /**
+     * Create an instance of {@link Listar }
+     * 
+     */
+    public Listar createListar() {
+        return new Listar();
+    }
+
+    /**
+     * Create an instance of {@link ListarResponse }
+     * 
+     */
+    public ListarResponse createListarResponse() {
+        return new ListarResponse();
+    }
+
+    /**
+     * Create an instance of {@link Categoria }
+     * 
+     */
+    public Categoria createCategoria() {
+        return new Categoria();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Exception }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://WebService/", name = "Exception")
+    public JAXBElement<Exception> createException(Exception value) {
+        return new JAXBElement<Exception>(_Exception_QNAME, Exception.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Listar }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://WebService/", name = "listar")
+    public JAXBElement<Listar> createListar(Listar value) {
+        return new JAXBElement<Listar>(_Listar_QNAME, Listar.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ListarResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://WebService/", name = "listarResponse")
+    public JAXBElement<ListarResponse> createListarResponse(ListarResponse value) {
+        return new JAXBElement<ListarResponse>(_ListarResponse_QNAME, ListarResponse.class, null, value);
+    }
+
+}
---
+++ src/java/Modelo/CategoriaService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import webservice.Exception_Exception;
+
+/**
+ *
+ * @author aries
+ */
+public class CategoriaService {
+
+    public CategoriaService() {
+    }
+
+    public java.util.List<webservice.Categoria> listar() throws Exception_Exception {
+        webservice.Servicios_Service service = new webservice.Servicios_Service();
+        webservice.Servicios port = service.getServiciosPort();
+        return port.listar();
+    }
+    
+}
